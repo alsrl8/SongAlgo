13da5f2c205f27dcd7837bba186bf92e47a6eddc
alsrl8 <55830760+alsrl8@users.noreply.github.com>
2023-09-13 18:47:53 +0900

    230913

---
+++ 230913/A → B.py
@@ -0,0 +1,19 @@
+import sys
+
+A, B = map(int, sys.stdin.readline().split())
+
+cnt = 0
+while B > A:
+    if B % 10 == 1:
+        B //= 10
+        cnt += 1
+    elif B % 2 == 0:
+        B //= 2
+        cnt += 1
+    else:
+        break
+
+if B == A:
+    print(cnt + 1)
+else:
+    print(-1)
