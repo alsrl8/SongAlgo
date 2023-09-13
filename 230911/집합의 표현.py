a6f8a4c977d30c60ebf94dc0185956f7623328a4
alsrl8 <55830760+alsrl8@users.noreply.github.com>
2023-09-13 10:51:49 +0900

    230913

--- 230911/집합의 표현.py
+++ 230911/집합의 표현.py
@@ -1,35 +1,31 @@
 import sys
 
+# Python 재귀의 최대 깊이를 지정한다.
+sys.setrecursionlimit(100000)
+
 n, m = map(int, sys.stdin.readline().split())
-parents = [num for num in range(n + 1)]
+
+# 처음 부모는 자기 자신으로 정한다.
+parents = [i for i in range(n + 1)]
 
 
-def union(a, b):
-    pa, pb = find(a), find(b)
-    if pa == pb:
-        return
-    else:
-        if pa < pb:
-            parents[pa] = pb
-        else:
-            parents[pb] = pa
+def find_parent(num: int) -> int:
+    if parents[num] != num:
+        ancestor = find_parent(parents[num])
+        parents[num] = ancestor
+    return parents[num]
 
 
-def find(num):
-    if parents[num] == num:
-        return num
-    else:
-        parents[num] = find(parents[num])
-        return parents[num]
-
-
+# Union-find
 for _ in range(m):
     cmd, a, b = map(int, sys.stdin.readline().split())
     if cmd == 0:
-        union(a, b)
+        pa, pb = find_parent(a), find_parent(b)
+        pa, pb = max(pa, pb), min(pa, pb)
+        parents[pa] = pb
     elif cmd == 1:
-        pa, pb = find(a), find(b)
-        if pa == pb:
-            print('YES')
-        else:
+        pa, pb = find_parent(a), find_parent(b)
+        if pa != pb:
             print('NO')
+        else:
+            print('YES')
