10dec7e533c0e356cffadc9a4b54ec04b226475e
alsrl8 <55830760+alsrl8@users.noreply.github.com>
2023-10-27 15:27:59 +0900

    231027

--- 231027/회전하는 큐.py
+++ 231027/회전하는 큐.py
@@ -22,3 +22,34 @@ def count_left(pos, tgt):
         cnt += 1
         while True:
             pos = (pos - 1) % N
+            if not visited[pos]:
+                break
+    return cnt
+
+
+def count_right(pos, tgt):
+    cnt = 0
+    while pos != tgt:
+        cnt += 1
+        while True:
+            pos = (pos + 1) % N
+            if not visited[pos]:
+                break
+    return cnt
+
+
+answer = 0
+for i, tgt in enumerate(order):
+    tgt -= 1
+    if cur == tgt:
+        if i == M - 1:
+            break
+        cur = go_right(cur)
+        continue
+
+    cl = count_left(cur, tgt)
+    cr = count_right(cur, tgt)
+    answer += min(cl, cr)
+    cur = go_right(tgt)
+
+print(answer)
