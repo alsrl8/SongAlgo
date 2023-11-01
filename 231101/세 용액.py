70bbdf99ace736f790a4304ed520fb5204264cf6
alsrl8 <55830760+alsrl8@users.noreply.github.com>
2023-11-01 19:34:58 +0900

    231101

--- 231101/세 용액.py
+++ 231101/세 용액.py
@@ -9,3 +9,20 @@ answer = [arr[0], arr[1], arr[2]]
 abs_answer = abs(sum(answer))
 
 for i in range(N):
+    j = i + 1
+    k = N - 1
+    while j < k:
+        _sum = arr[i] + arr[j] + arr[k]
+        if abs(_sum) < abs_answer:
+            abs_answer = abs(_sum)
+            answer = [arr[i], arr[j], arr[k]]
+
+        if _sum < 0:
+            j += 1
+        elif _sum > 0:
+            k -= 1
+        else:
+            break
+
+answer.sort()
+print(answer[0], answer[1], answer[2])
